6c2e70c08f25d67bfb1ebfa8fe91a67cddf4ec88
Samuvel Reegan <samuelreegan372@gmail.com>
2025-08-11 22:33:34 +0530

    remove add

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,24 +1,23 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
-import CountryLanguageSelector from "./CountryLanguageSelector";
 import "./css/Navbar.css";
 import { FaCog, FaHome, FaUser } from "react-icons/fa";
 import { IoMenu } from "react-icons/io5";
 import { BsCart2 } from "react-icons/bs";
 
 export default function Navbar() {
-  const { t } = useTranslation();
-
   return (
     <>
+      {/* Top navbar - Text (desktop) */}
       <div className="navbar-top">
-        <NavLink to="/" end>{t("home")}</NavLink>
-        <NavLink to="/about">{t("about")}</NavLink>
-        <NavLink to="/settings">{t("settings")}</NavLink>
-        <CountryLanguageSelector />
+        <NavLink to="/" end>Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/settings">Settings</NavLink>
       </div>
+      {/* Spacer for top navbar (desktop only) */}
+      <div className="navbar-top-spacer"></div>
 
+      {/* Bottom navbar - Icons (mobile) */}
       <div className="navbar-bottom">
         <NavLink to="/" end className={({ isActive }) => (isActive ? "active" : "")}>
           <FaHome />
@@ -26,16 +25,18 @@ export default function Navbar() {
         <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>
           <FaUser />
         </NavLink>
-        <NavLink to="/setting" className={({ isActive }) => (isActive ? "active" : "")}>
-        <IoMenu />
+        <NavLink to="/menu" className={({ isActive }) => (isActive ? "active" : "")}>
+          <IoMenu />
         </NavLink>
-        <NavLink to="/setings" className={({ isActive }) => (isActive ? "active" : "")}>
-       <BsCart2 />
+        <NavLink to="/cart" className={({ isActive }) => (isActive ? "active" : "")}>
+          <BsCart2 />
         </NavLink>
         <NavLink to="/settings" className={({ isActive }) => (isActive ? "active" : "")}>
           <FaCog />
         </NavLink>
       </div>
+      {/* Spacer for bottom navbar (mobile only) */}
+      <div className="navbar-bottom-spacer"></div>
     </>
   );
 }
---
+++ src/layout/MyLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import { Outlet } from 'react-router-dom'
+
+const MyLayout = () => {
+  return (
+    <div>
+        <Navbar />
+        <Outlet />
+    </div>
+  )
+}
+
+export default MyLayout
