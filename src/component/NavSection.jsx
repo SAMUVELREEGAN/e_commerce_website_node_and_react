c93f87385c17c3cb898193826517d0c01adf51c7
propertiesinfinite <propertiesinfinitemadurai@gmail.com>
2025-08-12 18:15:41 +0530

    update NavSection

--- src/component/NavSection.jsx
+++ src/component/NavSection.jsx
@@ -1,7 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import "./css/Navbar.css";
 import { FaCog, FaHome, FaUser } from 'react-icons/fa';
 import { IoMenu } from 'react-icons/io5';
@@ -12,29 +11,15 @@ const NavSection = () => {
   return (
     <div> 
       {/* Top Navbar (Desktop) */}
-      <Navbar expand="lg" className="navbar-top">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+     <Navbar expand="lg" className="navbar-top">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/">Link</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
 
       {/* Bottom Navbar (Mobile) */}
       <div className="navbar-bottom">
